918b21ac5c76712fe841bfdc2eae30d7d8143f28
Zzyytw <113056905+Zzyytw@users.noreply.github.com>
2022-09-13 12:00:21 +0800

    week02

---
+++ week02-3.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int z;
+    z = 2;
+    printf("現在的z是%d\n",z);
+    z = 99;
+    printf("現在的z是%d\n",z);
+}
---
+++ week02-4.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("請用鍵盤輸入兩個整數:\n");
+    scanf("%d\n",&a);
+    scanf("%d\n",&b);
+    printf("你輸入的整數是%d %d\n",a,b);
+    printf("他們加起來是:%d\n",a+b);
+}
