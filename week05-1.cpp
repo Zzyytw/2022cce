b15cac27427fb7aeb8222067df9c7d59e3cdd308
Zzyytw <113056905+Zzyytw@users.noreply.github.com>
2022-10-04 11:44:38 +0800

    week05

---
+++ week05-1.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    int z=10;
+    while (z=10) printf("a¬O10\n");
+}
---
+++ week05-2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int z=10;
+    while (z>0) {
+        printf("a¬O%d,a>0\n");
+        z--;
+        }
+}
