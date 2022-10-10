474d6d7a84148bb236edc9c2e19727018333ef43
Zzyytw <113056905+Zzyytw@users.noreply.github.com>
2022-10-11 01:43:46 +0800

    week05

---
+++ week05-5.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    for (int i=0;i<3;i++){
+        printf ("a¬O%d,a>0\n",i);
+    }
+    for (int i=1;i<=0;i++){
+        printf ("a¬O%d,a>0\n",i);
+    }
+    for (int i=3;i>0;i--){
+        printf ("a¬O%d,a>0\n",i);
+    }
+}
