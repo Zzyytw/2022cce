2a11f4693ff7036ad65129f1fc49779189387ef3
Zzyytw <113056905+Zzyytw@users.noreply.github.com>
2022-11-08 11:43:03 +0800

    Delete week10-3.cpp

--- week10-3.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int a[4]={10,20,30,40};
-    for (int i=0;i<4;i++) printf("a[%d]:%d\n",i,a[i]);
-    printf("上面正著印，下面倒著印\n");
-    for (int i=3;i>=0;i--) printf("a[%d]:%d\n",i,a[i]);
-}
