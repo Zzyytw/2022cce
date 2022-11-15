4d7af6c7f5fef4fe178af1cdc1b79428a789574b
Zzyytw <jackyyang0317@gmail.com>
2022-11-15 12:08:05 +0800

    week02

---
+++ week01/week01.2.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("老師早安\n");
+    //我是補交的
+    ///就考駕照那個
+}
--- week02-3.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int z;
-    z = 2;
-    printf("現在的z是%d\n",z);
-    z = 99;
-    printf("現在的z是%d\n",z);
-}
